80f249a025766289953c165bbae7176acfc20796
anilagraceprakash <anilagracep@gmail.com>
2018-03-27 15:42:05 +0530

    file modified

--- Client.java
+++ Client.java
@@ -1,2 +1,8 @@
 import java.io.*;
 import java.net.*;
+public class Client
+{
+  public static void main(String[] args) throws Exception
+  {
+     Socket sock = new Socket("127.0.0.1", 3000);
+	 // reading from keyboard (keyRead object)
