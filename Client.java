d10c08b99467075f159a4049cc7296707efc6701
anilagraceprakash <anilagracep@gmail.com>
2018-03-27 15:43:13 +0530

    file modified

--- Client.java
+++ Client.java
@@ -6,3 +6,7 @@ public class Client
   {
      Socket sock = new Socket("127.0.0.1", 3000);
 	 // reading from keyboard (keyRead object)
+     BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+  	 // sending to client (pwrite object)
+     OutputStream ostream = sock.getOutputStream(); 
+     PrintWriter pwrite = new PrintWriter(ostream, true);
