09bd844dfc3b170879bfd3377652eb7d9cca3171
anilagraceprakash <anilagracep@gmail.com>
2018-04-03 13:19:12 +0530

    Client file modified

--- Client.java
+++ Client.java
@@ -15,4 +15,16 @@ public class Client
      BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
      System.out.println("Start the chitchat, type and press Enter key");
  
-     String receiveMessage, sendMessage;   
+     String receiveMessage, sendMessage;  
+      while(true)
+     {
+        sendMessage = keyRead.readLine();  // keyboard reading
+        pwrite.println(sendMessage);       // sending to server
+        pwrite.flush();                    // flush the data
+        if((receiveMessage = receiveRead.readLine()) != null) //receive from server
+        {
+            System.out.println("Server: " + receiveMessage); // displaying at prompt
+        }         
+      }               
+    }                    
+}           
